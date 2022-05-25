610b0d09ed4eefaabb102077569f6e45bb7ec292
Grant Fellows <gwfellows@icloud.com>
2022-05-24 20:08:13 -0700

    placeholder what we do

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-navbarcode = `<b><a href="#">ROBOKITS</a></b> &nbsp &nbsp <a href="./what_we_do.html">what we do</a> &nbsp &nbsp <a href="http://www.example.com">see the kits</a> &nbsp &nbsp <a href="http://www.example.com">tutorials</a> &nbsp &nbsp <a href="http://www.example.com">contact us</a> &nbsp &nbsp <a href="http://www.example.com">donate</a>`;
+navbarcode = `<b><a href="./index.html">ROBOKITS</a></b> &nbsp &nbsp <a href="./what_we_do.html">what we do</a> &nbsp &nbsp <a href="http://www.example.com">see the kits</a> &nbsp &nbsp <a href="http://www.example.com">tutorials</a> &nbsp &nbsp <a href="http://www.example.com">contact us</a> &nbsp &nbsp <a href="http://www.example.com">donate</a>`;
 window.onload = function() {
   document.getElementById("navbar").innerHTML = navbarcode;
 }
