7ff2d8c6f954af4b00ab3a3e1646b3db3d05e1ec
Grant Fellows <gwfellows@icloud.com>
2022-05-24 20:20:36 -0700

    placeholders

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-navbarcode = `<b><a href="./index.html">ROBOKITS</a></b> &nbsp &nbsp <a href="./what_we_do.html">what we do</a> &nbsp &nbsp <a href="./see_the_kits.html">see the kits</a> &nbsp &nbsp <a href="./tutorials.html">tutorials</a> &nbsp &nbsp <a href="./contact.html">contact us</a> &nbsp &nbsp <a href="./donate.html">donate</a>`;
+navbarcode = `<b><a href="./index.html">ROBOKITS</a></b> &nbsp &nbsp <a href="./what_we_do.html">what we do</a> &nbsp &nbsp <a href="./see_the_kits.html">see the kits</a> &nbsp &nbsp <a href="./tutorials.html">tutorials</a> &nbsp &nbsp <a href="./contact_us.html">contact us</a> &nbsp &nbsp <a href="./donate.html">donate</a>`;
 window.onload = function() {
   document.getElementById("navbar").innerHTML = navbarcode;
 }
