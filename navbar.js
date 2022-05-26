4e0abebcded12203fc81205e3cda0f76f0543d88
Grant Fellows <gwfellows@icloud.com>
2022-05-26 10:37:47 -0700

    working dropdown

--- navbar.js
+++ navbar.js
@@ -1,4 +1,15 @@
-navbarcode = `<b><a href="./index.html">ROBOKITS</a></b> &nbsp &nbsp <a href="./what_we_do.html">what we do</a> &nbsp &nbsp <a href="./see_the_kits.html">see the kits</a> &nbsp &nbsp <a href="./tutorials.html">tutorials</a> &nbsp &nbsp <a href="./contact_us.html">contact us</a> &nbsp &nbsp <a href="./donate.html">donate</a>`;
+navbarcode = `<b><a href="./index.html">ROBOKITS</a></b> &nbsp &nbsp
+<a href="./what_we_do.html">what we do</a> &nbsp &nbsp
+<div class="dropdown">
+  <a href="./see_the_kits.html">see the kits</a>
+  <div class="dropdown-content">
+  <a href="./see_the_kits.html">kit 1</a>
+  <a href="./see_the_kits.html">kit 2</a>
+  <a href="./see_the_kits.html">kit 3</a>
+  </div>
+</div> &nbsp &nbsp
+<a href="./contact_us.html">contact us</a> &nbsp &nbsp
+<a href="./donate.html">donate</a>`;
 window.onload = function() {
   document.getElementById("navbar").innerHTML = navbarcode;
 }
