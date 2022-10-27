52816f639540d816cc5a8a8e2dffda7754de9dcc
Grant Fellows <33993581+gwfellows@users.noreply.github.com>
2022-10-27 08:44:04 -0700

    add kit video

--- navbar.js
+++ navbar.js
@@ -4,8 +4,8 @@ navbarcode = `<b><a href="./index.html">ROBOKITS</a></b> &nbsp &nbsp
   <a href="./see_the_kits.html">see the kits</a>
   <div class="dropdown-content">
   <a href="./see_the_kits.html">kit 1</a>
-  <a href="./see_the_kits.html">kit 2</a>
-  <a href="./see_the_kits.html">kit 3</a>
+  <a href="./see_the_kits_placeholder.html">kit 2</a>
+  <a href="./see_the_kits_placeholder.html">kit 3</a>
   </div>
 </div> &nbsp &nbsp
 <a href="./contact_us.html">contact us</a> &nbsp &nbsp
